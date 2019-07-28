9877aef74e37d266b0572599daddcb93e61b585e
Ulisse Mini <ulisse.mini@gmail.com>
2019-07-28 14:25:11 +0200

    Style changes

--- spec/bqueue_spec.cr
+++ spec/bqueue_spec.cr
@@ -3,16 +3,22 @@ require "./spec_helper"
 describe BQueue do
   # TODO: Write tests
 
-  it "Removes items when max size is reached" do
+  it "removes items when max size is reached" do
     bq = BQueue::BQueue(Int32).new(2)
 
-    bq.push 1
-    bq.push 2
-    bq.push 3
+    bq.push 1, 2, 3
 
     bq.pop.should eq 3
     bq.pop.should eq 2
 
     expect_raises(IndexError) { bq.pop }
+  end
+
+  it "joins strings" do
+    bq = BQueue::BQueue(String).new(3)
+    bq.push "foo"
+    bq.push "bar"
+
+    bq.join("\n").should eq "foo\nbar"
   end
 end
